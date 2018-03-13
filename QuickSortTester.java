64fb1edca1588fd16d8bcda2de98b5329da2bf79
aleong1 <aleong1@stuy.edu>
2018-03-13 17:00:52 -0400

    added to tester

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -5,14 +5,37 @@
 //2018-03-14
 
 public class QuickSortTester{
+    private static int ave = 0;
+    
     public static void main(String[] args){
+	int sizearr = 100;
+	// randomly-generated arrays of sizearr distinct vals  --> starts at size 100 then it increments by 100 until it gets to 1000
+	while(sizearr < 1000){
+	    int trial = 1;
+	    ave = 0;
+	
+	    int[] arrN = new int[sizearr]; //size 100 array first
+	    for( int i = 0; i < arrN.length; i++ )
+		arrN[i] = i;
+	    while(trial <= 1000){ //for 1000 trials per size of array
+	
+		QuickSort.shuffle(arrN);
 
-
-
+		long startTime = System.nanoTime();   //start timing
+		QuickSort.qsort( arrN );
+		long estimatedTime = System.nanoTime() - startTime;   //end timing
+		
+		if(trial <= 10){   //only print the data for 10 trials
+		    System.out.println("Trial " + trial + ": " + estimatedTime + " nanoseconds.");
+		}
+		trial += 1;
+		ave += estimatedTime;   //but this is the average of 1000 trials
+	    }
+	    sizearr += 100;
+	    
+	    System.out.println("Average of 1000 trials of size " + sizearr + " is " + (ave/1000) + " nanoseconds.\n\n");
+	}
 
     }
-
-    
-
-
 }
+
